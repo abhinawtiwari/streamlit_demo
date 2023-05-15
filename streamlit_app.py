58d2e73b25b6ba2e700fd8b620e46c745a0890ea
Abhinaw Tiwari <abhinawtiwari@Abhinaws-MacBook-Pro.local>
2023-05-15 01:24:16 +0100

    update texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,11 +7,11 @@ df = pd.read_csv('reduced_chowdary.csv')
 add_sidebar = st.sidebar.selectbox('Dropdown label', ('Dropdown value 1','Dropdown value 2'))
 
 if add_sidebar == 'Dropdown value 1':
-    st.write("This is for dropdown value 1")    
+    st.write("This is for dropdown value 1. It shows a sample dataframe.")    
     st.dataframe(df)
     
 if add_sidebar == 'Dropdown value 2':
-    st.write("This is for dropdown value 2")
+    st.write("This is for dropdown value 2. It shows a plotly chart.")
     st.selectbox('Sample picker:', ['pick 1', 'pick 2'])
     
     # Create a sample DataFrame
